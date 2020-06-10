485f2d96acb742a4791efb8a538cdc455861e8d8
sarikadara <sarika.dara96@gmail.com>
2020-06-10 16:08:09 +0530

    strings

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict"
+
+let theString = 'Hello, my name is dara sarika';
+
+// startsWith()
+// endsWith()
+// includes()
+
+//console.log(theString.startsWith('Hello'));
+//console.log(theString.endsWith('sarika'));
+//console.log(theString.includes('name'));
+
+//Hex
+//console.log(0xFF);
+// Binary
+console.log(0b101011);
+// Octal
+console.log(0o543);
+
+console.log(Number.isFinite(3));
+console.log(Number.isNaN('oct'));
+console.log(Number.isInteger(-2));
